4d996e56e23177977e2ef02ff44bfa1139448c92
dingxiangfei <dingxiangfei@linux-mgy8.site>
2015-09-13 20:41:13 +0800

    Initial database scheme

---
+++ backend/database/migrations/2015_09_12_163959_create_ride.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRide extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rides', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('initiator')
+              ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->json('descriptorJSON');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rides');
+    }
+}
