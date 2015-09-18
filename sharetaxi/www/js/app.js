46f4c74413a461fe8b3bcde14bfe714ba423d025
Naomi Leow <naomileowwx@gmail.com>
2015-09-19 00:48:40 +0800

    set up client side fb login

--- sharetaxi/www/js/app.js
+++ sharetaxi/www/js/app.js
@@ -1,5 +1,5 @@
 // App entrance
-angular.module('sharetaxi', ['ionic', 'st.map', 'st.selector', 'st.toolbar', 'st.results', 'ngOpenFB'])
+angular.module('sharetaxi', ['ionic', 'st.map', 'st.selector', 'st.toolbar', 'st.results', 'ngOpenFB', 'st.user.service'])
   .constant('googleApiKey', 'AIzaSyAgiS9kjfOa_eZ_h9uhIrGukIp_TyMj-_M')
   .constant('fbAppId', '1919268798299218')
   .config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider){
@@ -38,12 +38,13 @@ angular.module('sharetaxi', ['ionic', 'st.map', 'st.selector', 'st.toolbar', 'st
     }
 
   });
-}).controller('mainCtrl', ['googleApiKey', '$scope', '$ionicSideMenuDelegate',
-              function(googleApiKey, $scope, $ionicSideMenuDelegate){
+}).controller('mainCtrl', ['googleApiKey', '$scope', '$ionicSideMenuDelegate', 'userService',
+              function(googleApiKey, $scope, $ionicSideMenuDelegate, userService){
   GoogleMapsLoader.KEY = googleApiKey;
   GoogleMapsLoader.LIBRARIES = ['places'];
 
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
+  $scope.login = userService.fbLogin;
 }]);
--- sharetaxi/www/services/userService.js
+++ sharetaxi/www/services/userService.js
@@ -1,8 +1,59 @@
 /**
  * Created by naomileow on 18/9/15.
  */
-angular.module('st.user.service', [])
-.factory('userService', function($http){
+angular.module('st.user.service', ['ngOpenFB'])
+.factory('userService', function($http, ngFB){
+    var userData = {
+      accessToken: '',
+      name: '',
+      userID: ''
+    };
 
+    function loginToBackend(response){
+
+    }
+
+    function logoutFromBackend(){
+
+    }
+
+    function getUserDataFromFacebook(cb){
+      ngFB.api({path:'/me'}).then(function (response) {
+        userData.name = response.name;
+        userData.userID = response.id;
+        cb(response);
+      })
+    }
+    return {
+      fbLogin: function(){
+        ngFB.login({scope: 'email, user_friends'}).then(
+          function(response){
+            console.log(response);
+            if (response.status === 'connected') {
+              userData.accessToken = response.authResponse.accessToken;
+              getUserDataFromFacebook(loginToBackend);
+            } else {
+              console.log('Facebook login failed');//TODO:
+            }
+          }
+        )
+      },
+      fbLogout: function(){
+        ngFB.logout().then(
+          function(response){
+            logoutFromBackend();
+          }
+        );
+      },
+      getUserId: function(){
+        return userData.userID;
+      },
+      getName: function(){
+        return userData.name;
+      },
+      getAccessToken: function(){
+        return userData.accessToken;
+      }
+    }
 
   });
