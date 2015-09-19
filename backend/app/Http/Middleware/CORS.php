5250318f447095acbad5798210d000da74d2b108
Naomi Leow <naomileowwx@gmail.com>
2015-09-19 08:14:21 +0800

    removed cors header insertion for now because of errors

--- backend/app/Http/Middleware/CORS.php
+++ backend/app/Http/Middleware/CORS.php
@@ -15,8 +15,6 @@ class CORS implements Middleware {
   */
  public function handle($request, Closure $next)
  {
-  return $next($request)->header('Access-Control-Allow-Origin' , '*')
-          ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-          ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+  return $next($request);
  }
 }
