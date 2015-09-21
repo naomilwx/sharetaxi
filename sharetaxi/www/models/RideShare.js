60935052fc3f8e5a50c022f54e61bdb3ab8275de
Naomi Leow <naomileowwx@gmail.com>
2015-09-21 13:35:54 +0800

    change dep to arr

--- sharetaxi/www/models/RideShare.js
+++ sharetaxi/www/models/RideShare.js
@@ -1,11 +1,17 @@
-angular.module('models.rideshare', [])
-.factory('RideShare', function($http){
+angular.module('models.rideshare', ['models.route'])
+.factory('RideShare', function($http, Route){
   function RideShare(){
     //attributes: owner, riders, route
+    this.riders = [];
+    this.route = new Route();
   }
 
   RideShare.prototype.toBackendObject = function(){
 
   };
+
+  RideShare.buildFromBackendObject = function(obj) {
+
+  }
   return RideShare;
 });
--- sharetaxi/www/models/SharingOptions.js
+++ sharetaxi/www/models/SharingOptions.js
@@ -5,21 +5,21 @@ angular.module('models.sharingoptions', [])
     this.setCurrentDate();
   }
 
-  function constructDepartureDate(dep_date, dep_time){
-    return new Date(dep_date.getFullYear(), dep_date.getMonth(),
-     dep_date.getDate(), dep_time.getHours(), dep_time.getMinutes(),
-      dep_time.getSeconds(), dep_time.getMilliseconds());
+  function constructArrivalDate(arr_date, arr_time){
+    return new Date(arr_date.getFullYear(), arr_date.getMonth(),
+     arr_date.getDate(), arr_time.getHours(), arr_time.getMinutes(),
+      arr_time.getSeconds(), arr_time.getMilliseconds());
   };
 
   SharingOptions.prototype.setCurrentDate = function(){
-    this.dep_date = new Date();
-    this.dep_time = new Date();
-    this.dep_time.setMinutes((this.dep_date.getMinutes() + 15));
+    this.arr_date = new Date();
+    this.arr_time = new Date();
+    this.arr_time.setMinutes((this.arr_date.getMinutes() + 15));
   };
 
   SharingOptions.prototype.toBackendObject = function(){
     return {
-      departure_time: constructDepartureDate(this.dep_date, this.dep_time),
+      arrival_time: constructArrivalDate(this.arr_date, this.arr_time),
       notes: this.notes
     }
   };
