04211e8836a528fe78263e4f2b7c0a2534075e5b
Naomi Leow <naomileowwx@gmail.com>
2015-09-22 17:52:08 +0800

    refactored map view and add starting position marker

---
+++ sharetaxi/www/viewmodels/Map.js
@@ -0,0 +1,100 @@
+/**
+ * Created by naomileow on 22/9/15.
+ */
+angular.module('vm.map', ['st.service'])
+.factory('MapVM', function(displayService, placeService){
+    //Stores the map view, location markers and route renderers
+    var view = {
+      directionRenders: [],
+      mapMarkers: {}
+    }
+
+    function setMap(map){
+      view.map = map;
+    }
+
+    function getMap(){
+      return view.map;
+    }
+
+    function setPosition(myLatLng){
+      view.position = myLatLng;
+      placeService.getPlace(myLatLng, function(place){
+        view.currentPlace = place;
+      })
+    }
+
+    function getPosition(){
+      return view.position;
+    }
+
+    function displayDirections(directions){
+      //TODO: refactor this process
+      displayService.displayDirections(view.directionRenders, view.map, directions);
+    }
+
+    function clearDirections(){
+      displayService.clearDirections(view.directionRenders);
+      view.directionRenders = [];
+    }
+
+    function addPositionMarker(){
+      var place = view.currentPlace;
+      console.log(pos);
+      if(!view.positionMarker && place){
+        view.positionMarker = displayService.addMarker(place, view.map);
+      }else{
+        var pos = view.position;
+        placeService.getPlace(pos, function(place){
+          view.currentPlace = place;
+          view.positionMarker = displayService.addMarker(place, view.map);
+        })
+      }
+    }
+
+    function removePositionMarker(){
+      var marker = view.positionMarker;
+      if(marker){
+        displayService.removeMarker(marker);
+      }
+    }
+    function addMarker(place) {
+      var id = place.place_id;
+      var markers = view.mapMarkers;
+      if(markers[id] === undefined){
+        var marker = displayService.addMarker(place, view.map);
+        if(marker){
+          markers[id] = marker;
+        }
+      }
+    }
+
+    function removeMarker(place) {
+      var marker = view.mapMarkers[place.place_id];
+      if(marker){
+        displayService.removeMarker(marker);
+        delete view.mapMarkers[place.place_id];
+      }
+    }
+
+    function clearMarkers(){
+      for(var id in view.mapMarkers) {
+        var marker = view.mapMarkers[id];
+        displayService.removeMarker(marker);
+      }
+    }
+
+    return {
+      setMap: setMap,
+      getMap: getMap,
+      setPosition: setPosition,
+      getPosition: getPosition,
+      addPositionMarker: addPositionMarker,
+      removePositionMarker: removePositionMarker,
+      clearDirections: clearDirections,
+      displayDirections: displayDirections,
+      addMarker: addMarker,
+      removeMarker: removeMarker,
+      clearMarkers: clearMarkers
+    }
+  });
