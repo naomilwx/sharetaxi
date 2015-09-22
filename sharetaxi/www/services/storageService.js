627de195c74c6147facf02c180867fc57cf16ab6
Naomi Leow <naomileowwx@gmail.com>
2015-09-23 01:11:00 +0800

    with basic queries for index db

--- sharetaxi/www/services/storageService.js
+++ sharetaxi/www/services/storageService.js
@@ -1,36 +1,98 @@
 /**
  * Created by naomileow on 22/9/15.
  */
-angular.module('st.storage', ['indexedDB'])
-.factory('storageService', function($indexedDB){
-  function saveRoute(route){
-    return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
-      store.insert(route);
-    });
-  }
+angular.module('st.storage', ['indexedDB', 'ngStorage'])
+.factory('storageService', function($indexedDB, $localStorage){
+    function saveRoute(route){
+      return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
+        return store.insert(route);
+      });
+    }
 
-  function updateRoute(route){
-    return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
-      store.upsert(route);
-    });
-  }
+    function getAllRoutesForUser() {
+      return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
+        var query = store.query();
+        query.index('creator_idx');
+        query.asc();
+        if($localStorage.user){
+          query.eq($localStorage.user.user_id);
+        }
 
-  function getRouteById(routeId){
-    return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
-      store.findBy("route_id_idx", routeId);
-    });
-  }
+        store.findWhere(query)
+      });
+    }
+    function updateRoute(route){
+      return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
+        return store.upsert(route);
+      });
+    }
 
-  function getRouteByLocalId(localId){
-    return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
-      store.find({"local_id":localId}); //TODO: check this
-    });
-  }
+    function getRouteById(routeId){
+      return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
+        return store.findBy("route_id_idx", routeId);
+      });
+    }
 
-  return {
+    function getRouteByLocalId(localId){
+      return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
+        return store.find(localId);
+      });
+    }
+
+    function deleteRoute(localId){
+      return $indexedDB.openStore(ROUTE_STORE_NAME, function(store) {
+        return store.delete(localId);
+      });
+    }
+
+    function saveRideShare(rideShare){
+      return $indexedDB.openStore(RIDESHARE_STORE_NAME, function(store) {
+        store.insert(rideShare);
+      });
+    }
+
+    function updateRideShare(rideShare){
+      return $indexedDB.openStore(RIDESHARE_STORE_NAME, function(store) {
+        return store.upsert(rideShare);
+      });
+    }
+
+    function getRideShareById(id){
+      return $indexedDB.openStore(RIDESHARE_STORE_NAME, function(store) {
+        return store.find(id);
+      });
+    }
+
+    function getRideShareForRoute(route) {
+      return $indexedDB.openStore(RIDESHARE_STORE_NAME, function(store) {
+        return store.findBy('route_idx', route.route_id);
+      });
+    }
+
+    function getRideShareByOwner(owner) {
+      return $indexedDB.openStore(RIDESHARE_STORE_NAME, function(store) {
+        return store.findBy('owner_idx', owner.user_id);
+      })
+    }
+
+    function deleteRideShare(id) {
+      return $indexedDB.openStore(RIDESHARE_STORE_NAME, function(store) {
+        return store.delete(id);
+      });
+    }
+
+    return {
     saveRoute: saveRoute,
     updateRoute: updateRoute,
     getRouteById: getRouteById,
-    getRouteByLocalId: getRouteByLocalId
+    getRouteByLocalId: getRouteByLocalId,
+      getAllRoutesForUser: getAllRoutesForUser,
+      deleteRoute: deleteRoute,
+      saveRideShare: saveRideShare,
+      updateRideShare: updateRideShare,
+      getRideShareById: getRideShareById,
+      getRideShareForRoute: getRideShareForRoute,
+      getRideShareByOwner: getRideShareByOwner,
+      deleteRideShare: deleteRideShare
   }
   });
