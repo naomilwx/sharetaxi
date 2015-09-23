2b750f14a765e9f32936851f77bb822bbe5f22e9
yzxj <yeozx@comp.nus.edu.sg>
2015-09-23 12:42:39 +0800

    Completed intro

--- sharetaxi/www/components/intro/intro.js
+++ sharetaxi/www/components/intro/intro.js
@@ -1,10 +1,10 @@
 // Adapted from http://codepen.io/gwhickman/pen/zpDFG
 
 angular.module('st.intro', ['ionic', 'ngAnimate'])
-.controller('introCtrl', ['$scope', '$state', '$timeout', 
+.controller('introCtrl', ['$scope', '$state', '$timeout',
             function($scope, $state, $timeout){
   $scope.showSplash = true;
-  $scope.showTutorial = false;
+  $scope.showIntro = false;
 
   // Called to navigate to the main app
   $scope.startApp = function() {
@@ -20,81 +20,65 @@ angular.module('st.intro', ['ionic', 'ngAnimate'])
           $scope.startApp();
       } else {
           $scope.showSplash = false;
-          $scope.showTutorial = true;
+          $scope.showIntro = true;
       }
     }, 1000);
   });
   
   // Move to the next slide
-  $scope.nextIntroSlide = function() {
+  var nextSlide = function() {
     $scope.$broadcast('slideBox.nextSlide');
   };
-
-  // Our initial right buttons
-  var rightButtons = [
-    {
-      content: 'Next',
-      type: 'button-positive button-clear',
-      tap: function(e) {
-        // Go to the next slide on tap
-        $scope.nextIntroSlide();
-      }
-    }
-  ];
-  
-  // Our initial left buttons
-  var leftButtons = [
-    {
-      content: 'Skip',
-      type: 'button-positive button-clear',
-      tap: function(e) {
-        // Start the app on tap
-        startApp();
-      }
-    }
-  ];
-
-  // Bind the left and right buttons to the scope
-  $scope.leftButtons = leftButtons;
-  $scope.rightButtons = rightButtons;
-
-
-  // Called each time the slide changes
-  $scope.slideChanged = function(index) {
-
-    // Check if we should update the left buttons
-    if(index > 0) {
-      // If this is not the first slide, give it a back button
-      $scope.leftButtons = [
-        {
-          content: 'Back',
-          type: 'button-positive button-clear',
-          tap: function(e) {
-            // Move to the previous slide
-            $scope.$broadcast('slideBox.prevSlide');
-          }
-        }
-      ];
-    } else {
-      // This is the first slide, use the default left buttons
-      $scope.leftButtons = leftButtons;
-    }
-    
-    // If this is the last slide, set the right button to
-    // move to the app
-    if(index == 2) {
-      $scope.rightButtons = [
-        {
-          content: 'Start using ShareTaxi',
-          type: 'button-positive button-clear',
-          tap: function(e) {
-            startApp();
-          }
-        }
-      ];
-    } else {
-      // Otherwise, use the default buttons
-      $scope.rightButtons = rightButtons;
-    }
+  // Move to the next slide
+  var prevSlide = function() {
+    $scope.$broadcast('slideBox.prevSlide');
   };
+
+  var slideIndex = 1;
+  $scope.leftButton = "Skip";
+  $scope.rightButton = "Next";
+
+  $scope.leftButtonTap = function() {
+    $scope.startApp();
+  };
+
+  // // Called each time the slide changes
+  // $scope.slideChanged = function(index) {
+  //   slideIndex = index;
+  //   console.log(index);
+  //   Check if we should update the left buttons
+  //   if(index > 0) {
+  //     // If this is not the first slide, give it a back button
+  //     $scope.leftButtons = [
+  //       {
+  //         content: 'Back',
+  //         type: 'button-positive button-clear',
+  //         tap: function(e) {
+  //           // Move to the previous slide
+  //           $scope.$broadcast('slideBox.prevSlide');
+  //         }
+  //       }
+  //     ];
+  //   } else {
+  //     // This is the first slide, use the default left buttons
+  //     $scope.leftButtons = leftButtons;
+  //   }
+    
+  //   // If this is the last slide, set the right button to
+  //   // move to the app
+  //   if(index == 2) {
+  //     $scope.rightButtons = [
+  //       {
+  //         content: 'Start using ShareTaxi',
+  //         type: 'button-positive button-clear',
+  //         tap: function(e) {
+  //           startApp();
+  //         }
+  //       }
+  //     ];
+  //   } else {
+  //     // Otherwise, use the default buttons
+  //     $scope.rightButtons = rightButtons;
+  //   }
+  // };
 }]);
